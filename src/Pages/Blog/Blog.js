d7b0111b2484fab2f0bbae4b3a96799f37d97fd1
anikkumer <anikkumerdey56@gmail.com>
2022-11-09 00:24:00 +0600

    error page create

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -15,39 +15,51 @@ const Blog = () => {
           are non-relational and generally do not use SQL.
         </p>
       </div>
-      <div className="collapse-title text-xl font-medium">
-        What is JWT, and how does it work?
+
+      <div className="collapse">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          What is JWT, and how does it work?
+        </div>
+        <div className="collapse-content">
+          <p>
+            JWT, or JSON Web Token, is an open standard used to share security
+            information between two parties — a client and a server. Each JWT
+            contains encoded JSON objects, including a set of claims. JWTs are
+            signed using a cryptographic algorithm to ensure that the claims
+            cannot be altered after the token is issued.
+          </p>
+        </div>
       </div>
-      <div className="collapse-content">
-        <p>
-          JWT, or JSON Web Token, is an open standard used to share security
-          information between two parties — a client and a server. Each JWT
-          contains encoded JSON objects, including a set of claims. JWTs are
-          signed using a cryptographic algorithm to ensure that the claims
-          cannot be altered after the token is issued.
-        </p>
+
+      <div className="collapse">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          What is the difference between javascript and NodeJS?
+        </div>
+        <div className="collapse-content">
+          <p>
+            JavaScript is a simple programming language that can be used with
+            any browser that has the JavaScript Engine installed. Node. js, on
+            the other hand, is an interpreter or execution environment for the
+            JavaScript programming language.
+          </p>
+        </div>
       </div>
-      <div className="collapse-title text-xl font-medium">
-        What is the difference between javascript and NodeJS?
-      </div>
-      <div className="collapse-content">
-        <p>
-          JavaScript is a simple programming language that can be used with any
-          browser that has the JavaScript Engine installed. Node. js, on the
-          other hand, is an interpreter or execution environment for the
-          JavaScript programming language.
-        </p>
-      </div>
-      <div className="collapse-title text-xl font-medium">
-        How does NodeJS handle multiple requests at the same time?
-      </div>
-      <div className="collapse-content">
-        <p>
-          NodeJS receives multiple client requests and places them into
-          EventQueue. NodeJS is built with the concept of event-driven
-          architecture. NodeJS has its own EventLoop which is an infinite loop
-          that receives requests and processes them.
-        </p>
+
+      <div className="collapse">
+        <input type="checkbox" />
+        <div className="collapse-title text-xl font-medium">
+          How does NodeJS handle multiple requests at the same time?
+        </div>
+        <div className="collapse-content">
+          <p>
+            NodeJS receives multiple client requests and places them into
+            EventQueue. NodeJS is built with the concept of event-driven
+            architecture. NodeJS has its own EventLoop which is an infinite loop
+            that receives requests and processes them.
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/Router/Routes/Routes.js
+++ src/Router/Routes/Routes.js
@@ -1,8 +1,33 @@
+import Main from "../../Layout/Main";
+import Blog from "../../Pages/Blog/Blog";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
+import Home from "../../Pages/Home/Home";
+import Services from "../../Pages/Services/Services";
+
 const { createBrowserRouter } = require("react-router-dom");
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/home",
+        element: <Home></Home>,
+      },
+      {
+        path: "/services",
+        element: <Services></Services>,
+      },
+      {
+        path: "/blog",
+        element: <Blog></Blog>,
+      },
+    ],
+  },
+  {
+    path: "/*",
+    element: <ErrorPage></ErrorPage>,
   },
 ]);
 
